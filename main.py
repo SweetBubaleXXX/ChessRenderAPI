3ab40f2a7119bc936ef735779b603fcfaaf5080d
arakamboob <1pcpcpc1pc@gmail.com>
2022-06-05 15:26:32 +0300

    Added GET method to /render

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from render import Render, config
 
 render_engines = {size: Render(size) for size in config.SIZES}
 
-Iterable[int]
-
 
 class Board(BaseModel):
     field: list[list[str]] = [
@@ -31,7 +29,7 @@ class Board(BaseModel):
     check: Optional[list[Iterable[int]]]
 
 
-app = FastAPI()
+app = FastAPI(title="ChessRenderAPI")
 
 
 @app.get("/sizes", response_model=list[int])
@@ -39,8 +37,9 @@ async def sizes():
     return config.SIZES
 
 
-@app.post("/convert")
-async def convert(size: int = config.SIZES[0], board: Board = Body()):
+@app.get("/render")
+@app.post("/render")
+async def convert(size: int = config.SIZES[0], board: Board = Body(default=Board())):
     if size not in config.SIZES:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid size")
